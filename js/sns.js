68cfbb6a4b7d39fb693e9ff20157f3a6e88193e0
tamezolab-cpu <tamezolab@gmail.com>
2026-05-30 23:16:31 +0900

    新着表示が多いときに立てスクロールさせる

--- js/sns.js
+++ js/sns.js
@@ -1,1 +1,5 @@
 console.log("hello world");
+
+//260530 更新ボタンを押すとフォロワーの投稿が流れる
+const reload_btn = document.getElementById("reload_btn");
+const text_disp_area = document.getElementById("text_disp_area");
